6b31f84f13e1c751f790a8dc632c7504b28c2ef3
Esther Mamai <mamaiesther47@gmail.com>
2024-08-29 17:07:56 +0300

    list comprehension

--- week4.py
+++ week4.py
@@ -17,3 +17,21 @@ def is_palindrome(s):
 print(is_palindrome("radar"))
 print(is_palindrome("hello"))  
 print(is_palindrome("teapot"))
+
+"""List Comprehension
+List comprehension is a concise way to create lists in Python. 
+It allows for generating a new list by applying an expression to each item in an existing iterable."""
+
+#1. Basic list
+squares = [x**2 for x in range(10)]
+print(squares)  
+
+#2. List comrehension with a condition
+even_numbers = [x for x in range(20) if x % 2 == 0]
+print(even_numbers) 
+
+#3. Nested List Comprehension
+matrix = [[j for j in range(3)] for i in range(3)]
+print(matrix)  
+
+
